1d8a722ce2bf243eb30412940842efa82d368642
Thomas Simon <simoncreationweb@gmail.com>
2018-01-20 16:44:17 +0100

    Ajout du model enregistrement des email dans firebase

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,23 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: "AIzaSyAY48OZOou5dKTWBsbPwOYkatJnjmELvNo",
+      authDomain: "libraryapp-e493d.firebaseapp.com",
+      databaseURL: "https://libraryapp-e493d.firebaseio.com",
+      projectId: "libraryapp-e493d",
+      storageBucket: "libraryapp-e493d.appspot.com",
+      messagingSenderId: "330159609620"
+    },
+
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
