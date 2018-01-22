ee3690e382dcc3e3a060b3f3e123cb6c55268bbb
Thomas Simon <simoncreationweb@gmail.com>
2018-01-22 16:34:55 +0100

    exo2 options2

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,18 @@
 import DS from 'ember-data';
+import { match, gte ,and,not} from '@ember/object/computed';
 
 export default DS.Model.extend({
+
+
+  title:'Contact',
+
   email: DS.attr('string'),
   message: DS.attr('string'),
+
+  validEmail: match('email', /^.+@.+\..+$/),
+  validMessage: gte('message.length', 5),
+
+  isValid:and('validEmail','validMessage'),
+  isDisabled:not('isValid'),
+
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -4,4 +4,21 @@ export default Route.extend({
   model() {
     return this.store.createRecord('contact');
   },
+
+  actions: {
+
+    sendMessage(newContactMessage) {
+      newContactMessage.save().then(() => this.controller.set('responseMessage', true));
+    },
+
+    willTransition() {
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+
+      this.controller.set('responseMessage', false);
+    }
+  }
 });
